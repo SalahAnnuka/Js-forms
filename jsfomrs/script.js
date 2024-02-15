e9f1dc08fe08d0a4d11fedfc24e7183e383ce1ff
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-15 12:50:48 +0300

    UPDATE ALL

---
+++ jsfomrs/script.js
@@ -0,0 +1,11 @@
+function onChangeEmail(){
+    window.alert(document.getElementById("email").value);
+}
+
+function onChangePassword(){
+    window.alert(document.getElementById("password").value);
+}
+
+function onChangeTelephone(){
+    window.alert(document.getElementById("telephone").value);
+}
